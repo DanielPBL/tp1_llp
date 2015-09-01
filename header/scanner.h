30f4d10d6c666e23ea2de7ce645a8b9a9dc4541d
Daniel Lopes <danielllopes11@gmail.com>
2015-09-01 19:12:21 -0300

    Versão inicial do projeto

---
+++ header/scanner.h
@@ -0,0 +1,69 @@
+#ifndef __SCANNER_H__
+#define __SCANNER_H__
+
+#include <fstream>
+#include <string>
+#include <map>
+
+#define FIM_ARQ_INEXPERADO -2
+#define TOKEN_INVALIDO     -1
+#define FIM_ARQ_NORMAL      0
+#define TEMPO 				1
+#define MUSICA 				2
+#define FACA 				3
+#define REPETIR				4
+#define TOCAR 				5
+#define SE 					6
+#define SENAO 				7
+#define FIM 				8
+#define PAUSAR 				9
+#define VARIAVEL 		   10
+#define NOTA 			   11
+#define PONTO_VIRGULA 	   12
+#define IGUAL 			   13
+#define ABRE_PARENTESES    14
+#define FECHA_PARENTESES   15
+#define DOIS_PONTOS 	   16
+#define PONTO 			   17
+#define PORCENTO 		   18
+#define IGUAL_IGUAL 	   19
+#define DIFERENTE 		   20
+#define MENOR 			   21
+#define MAIOR 			   22
+#define IGUAL_MENOR 	   23
+#define MAIOR_IGUAL 	   24
+#define MAIS 			   25
+#define MENOS 			   26
+#define VEZES 			   27
+#define DIVIDIDO 		   28
+#define NUMERO			   29
+#define STRING 			   30
+#define VIRGULA			   31
+
+typedef struct LEXEMA {
+	std::string token;
+	int tipo;
+} Lexema;
+
+class Scanner {
+	private:
+		std::ifstream arquivo;
+		int linha;
+		std::map<std::string, int> tabelaSimbolos;
+	public:
+		Scanner(char* arquivo);
+		~Scanner();
+		Lexema getLexema();
+		int getLinha();
+		int consultarTabelaSimbolos(std::string);
+		void criarTabelaSimbolos();
+		void ungetChar(char);
+		static bool isDigit(char);
+		static bool isLowerLetter(char);
+		static bool isUperLetter(char);
+		static bool isLetter(char);
+		static bool isBlank(char);
+		static bool isSimbolo(char);
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <list>
+#include "scanner.h"
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	if (argc < 2) {
+		cout << "Faltam argumentos!" << endl;
+		return EXIT_FAILURE;
+	}
+
+	try {
+		Scanner scanner(argv[1]);
+		Lexema lexema;
+
+		while ((lexema = scanner.getLexema()).tipo != FIM_ARQ_NORMAL) {
+			if (lexema.tipo < 0) {
+				switch (lexema.tipo) {
+					case FIM_ARQ_INEXPERADO:
+						cout << "Fim de arquivo inexperado!";
+						break;
+					case TOKEN_INVALIDO:
+						cout << "(" << lexema.token << ")" << "Token inválido!";
+						break;
+				}
+				cout << " - Linha: " << scanner.getLinha() << endl;
+			} else {
+				cout << "Token: " << lexema.token << " - Tipo: " << lexema.tipo << endl;
+			}
+		}
+	} catch (const char* msg) {
+		cout << msg;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
