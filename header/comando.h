427bef0b3b409dab77f302a2a76d0e052519836d
Daniel Lopes <danielllopes11@gmail.com>
2015-09-20 12:11:50 -0300

    Algumas novas classes

---
+++ header/comando.h
@@ -0,0 +1,10 @@
+#ifndef __COMANDO_H__
+#define __COMANDO_H__
+
+class Comando {
+public:
+
+  void executar()
+};
+
+#endif // ifndef __COMANDO_H__
---
+++ header/nota.h
@@ -0,0 +1,19 @@
+#ifndef __NOTA_H__
+#define __NOTA_H__
+
+enum class Nota : double {
+  La = 440.0,
+  LaSustenido = 466.2,
+  Si = 493.9,
+  Do = 261.6,
+  DoSustenido = 277.2,
+  Re = 293.7,
+  ReSustenido = 311.1,
+  Mi = 329.6,
+  Fa = 349.2,
+  FaSustenido = 370.0,
+  Sol = 392.0,
+  SolSustenido = 415.3
+}
+
+#endif
